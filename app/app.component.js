2743411edc6ba1f7b32c669caace667b12f9f752
Nano Molina <nanomolinacav@gmail.com>
2016-01-27 13:44:37 -0300

    lista de heroes + detalles de heroe

--- app/app.component.js
+++ app/app.component.js
@@ -9,7 +9,7 @@ System.register(['angular2/core'], function(exports_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var AppComponent, HEROES;
     return {
         setters:[
             function (core_1_1) {
@@ -19,21 +19,34 @@ System.register(['angular2/core'], function(exports_1) {
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
-                    this.hero = {
-                        id: 1,
-                        name: 'Windstorm'
-                    };
+                    this.heroes = HEROES;
                 }
+                AppComponent.prototype.onSelect = function (hero) {
+                    this.selectedHero = hero;
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1>{{title}}</h1>\n    <h2>{{hero.id}}: {{hero.name}} details!</h2>\n    <div>\n      <div class=\"form-group\">\n        <label for=\"hero-id\"><strong>id: </strong></label>\n        <input type=\"text\" id=\"hero.id\" [(ngModel)]=\"hero.id\" class=\"form-control\">\n      </div>\n      <div class=\"form-group\">\n        <label for=\"hero-name\"><strong>name: </strong></label>\n        <input type=\"text\" id=\"hero-name\" [(ngModel)]=\"hero.name\" class=\"form-control\" placeholder=\"name\">\n      </div>\n    </div>\n    "
+                        template: "\n    <h2>My Heroes</h2>\n    <ul class=\"list-group heroes\">\n      <li *ngFor=\"#hero of heroes\" class=\"list-group-item\" [class.active]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n          <span class=\"badge\">{{hero.id}}</span>\n          {{hero.name}}\n      </li>\n    </ul>\n\n\n    <div *ngIf=\"selectedHero\">\n      <hr>\n      <h2>{{title}}</h2>\n      <h3>{{selectedHero.id}}: {{selectedHero.name}} details!</h3>\n      <div>\n        <div class=\"form-group\">\n          <label for=\"hero-id\"><strong>id: </strong></label>\n          <input type=\"text\" id=\"hero.id\" [(ngModel)]=\"selectedHero.id\" class=\"form-control\">\n        </div>\n        <div class=\"form-group\">\n          <label for=\"hero-name\"><strong>name: </strong></label>\n          <input type=\"text\" id=\"hero-name\" [(ngModel)]=\"selectedHero.name\" class=\"form-control\" placeholder=\"name\">\n        </div>\n      </div>\n    </div>\n    ",
+                        styles: ["\n    .heroes li {\n      margin: .10em;\n      cursor: pointer;\n    }\n    .heroes li:hover {\n      border: 1px solid #df691a;\n    }\n    .badge {\n      border: 1px solid #df691a;\n    }\n  "],
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             })();
             exports_1("AppComponent", AppComponent);
+            HEROES = [
+                { "id": 11, "name": "Mr. Nice" },
+                { "id": 12, "name": "Narco" },
+                { "id": 13, "name": "Bombasto" },
+                { "id": 14, "name": "Celeritas" },
+                { "id": 15, "name": "Magneta" },
+                { "id": 16, "name": "RubberMan" },
+                { "id": 17, "name": "Dynama" },
+                { "id": 18, "name": "Dr IQ" },
+                { "id": 19, "name": "Magma" },
+                { "id": 20, "name": "Tornado" }
+            ];
         }
     }
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -8,25 +8,64 @@ interface Hero {
 @Component({
   selector: 'my-app',
   template:`
-    <h1>{{title}}</h1>
-    <h2>{{hero.id}}: {{hero.name}} details!</h2>
-    <div>
-      <div class="form-group">
-        <label for="hero-id"><strong>id: </strong></label>
-        <input type="text" id="hero.id" [(ngModel)]="hero.id" class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="hero-name"><strong>name: </strong></label>
-        <input type="text" id="hero-name" [(ngModel)]="hero.name" class="form-control" placeholder="name">
+    <h2>My Heroes</h2>
+    <ul class="list-group heroes">
+      <li *ngFor="#hero of heroes" class="list-group-item" [class.active]="hero === selectedHero" (click)="onSelect(hero)">
+          <span class="badge">{{hero.id}}</span>
+          {{hero.name}}
+      </li>
+    </ul>
+
+
+    <div *ngIf="selectedHero">
+      <hr>
+      <h2>{{title}}</h2>
+      <h3>{{selectedHero.id}}: {{selectedHero.name}} details!</h3>
+      <div>
+        <div class="form-group">
+          <label for="hero-id"><strong>id: </strong></label>
+          <input type="text" id="hero.id" [(ngModel)]="selectedHero.id" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="hero-name"><strong>name: </strong></label>
+          <input type="text" id="hero-name" [(ngModel)]="selectedHero.name" class="form-control" placeholder="name">
+        </div>
       </div>
     </div>
-    `
+    `,
+  styles:[`
+    .heroes li {
+      margin: .10em;
+      cursor: pointer;
+    }
+    .heroes li:hover {
+      border: 1px solid #df691a;
+    }
+    .badge {
+      border: 1px solid #df691a;
+    }
+  `],
 })
 
 export class AppComponent {
   public title = 'Tour of Heroes';
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  public heroes = HEROES;
+  public selectedHero: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
